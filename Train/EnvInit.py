bce04bb472c8aa5ec39a6df276736bc1e363b955
EMAJIANG <134196879+EMAJIANG@users.noreply.github.com>
2025-04-16 13:50:11 -0400

    Update EnvInit.py

--- Train/EnvInit.py
+++ Train/EnvInit.py
@@ -26,14 +26,14 @@ class EnvSetup:
 
         ##### add asset court,robot,ground reference to the world
         current_path = os.getcwd()
-        Ground_asset_path = current_path +"/Tennis-Robot-Code-main/GroundPlane.usd"
+        Ground_asset_path = current_path +"/GroundPlane.usd"
         add_reference_to_stage(usd_path=Ground_asset_path, prim_path="/World/GroundPlane")
 
-        TR_asset_path = current_path + "/Tennis-Robot-Code-main/TR_V4.usd"
+        TR_asset_path = current_path + "/TR_V4.usd"
         add_reference_to_stage(usd_path=TR_asset_path, prim_path="/World/TR_V4")
         self.TR_V4 = self.ros_world.scene.add(Robot(prim_path="/World/TR_V4", name="TR_V4"))
 
-        Court_asset_path = current_path + "/Tennis-Robot-Code-main/Court.usd"
+        Court_asset_path = current_path + "/Court.usd"
         add_reference_to_stage(usd_path=Court_asset_path, prim_path="/World/Court")
         Court = self.ros_world.scene.add(XFormPrim(prim_paths_expr="/World/Court", 
                                                 name="Court",
